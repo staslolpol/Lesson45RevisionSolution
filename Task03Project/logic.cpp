4218d921aec1c129c42f8c0c72f30e8f7790fbb8
staslolpol <staspriho9@gmail.com>
2026-03-02 17:37:52 +0300

    таск 3 готов

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -4,3 +4,19 @@
 // Дан вектор целочисленных значений. Необходимо спроектировать 
 // и реализовать функцию, которая проверяет, что среди значений 
 // элементов вектора есть хотя бы одна пара совпадающих.
+
+#include "logic.h"
+
+
+bool is_the_same_elements(int array[DEFAULT_SIZE], int size) {
+	for (int i = 0; i < size; i++)
+	{
+		for (int n = size; n > 0; n--)
+		{
+			if (array[i] == array[n] && i!=n) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
