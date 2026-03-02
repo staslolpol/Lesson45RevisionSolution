6bca06f7cf55c4008bdbb61e9ed69bcba8b59657
staslolpol <staspriho9@gmail.com>
2026-03-02 18:45:44 +0300

    task 05 cjmplitre

--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -4,3 +4,29 @@
 // Дан вектор целочисленных значений. Необходимо спроектировать 
 // и реализовать функцию, которая реверсирует элементы вектора 
 // начиная с позиции a и заканчивая позицией b, включая данные позиции.
+#include "logic.h"
+
+void reverse(int array[DEFAULT_SIZE], int size, int a, int b) {
+	int temp;
+
+	if (a < 0 || b < 0 || b >= size || size <= 0 || a >= size) {
+		return;
+	}
+
+	if (a > b) {
+		int temp = a;
+		a = b;
+		b = temp;
+
+	}
+	
+	int n = (a + b) / 2;
+	for (int i = a; i <= n; i++)
+	{
+		temp = array[i];
+		array[i] = array[b];
+		array[b] = temp;
+		b--;
+	}
+
+}
